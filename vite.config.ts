1b2880eb8be33e15162a5bc17b09d978a0611281
giaonk <giaonk@gviet.vn>
2022-09-13 11:02:48 +0700

    feat: init config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Unocss from 'unocss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Unocss({ /* options */ }),
+    AutoImport({
+      imports: [
+        'vue',
+        '@vueuse/core',
+      ],
+      dts: './src/auto-imports.d.ts',
+      vueTemplate: true,
+      dirs: [],
+    }),
+  ]
 })
